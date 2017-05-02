c4ee4e810bfcad409a4fe33803eb879144a7597f
Nikolas Makiya Vichi <nikolasj5@gmail.com>
2017-05-02 15:58:37 -0300

    Added support for nicknames

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/html/index.html');
 });
 
 io.on('connection', function(socket){
 	console.log('a user connected');
-	io.emit('chat message', 'a user connected');
+
+	var nickname = '';
+	socket.emit('chat message', 'Say your name');
+
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
-		io.emit('chat message', 'user disconnected');
+		console.log('user disconnected: '+nickname);
+		io.emit('chat message', nickname+' disconnected');
 	})
+
 	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+		if (nickname == ''){
+			nickname = msg;
+			greet = "Thanks "+nickname+", now you can talk";
+			socket.emit('chat message', greet);
+		} else {
+			io.emit('chat message', nickname+":\n"+msg);
+		}
 	});
 });
 
