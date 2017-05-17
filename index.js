8d5d461411520e5d073a08035381e31e4d75f5fe
Nikolas Makiya Vichi <nikolasj5@gmail.com>
2017-05-17 17:24:16 -0300

    "Show who's online" feature added

--- index.js
+++ index.js
@@ -6,30 +6,44 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/html/index.html');
 });
 
+var users_online = [];
 io.on('connection', function(socket){
 	console.log('a user connected');
 
 	var nickname = '';
-	socket.emit('chat message', 'Say your name');
+	socket.emit('chat message', 'What\'s your name');
 
 	socket.on('disconnect', function(){
-		console.log('user disconnected: '+nickname);
-		io.emit('chat message', nickname+' disconnected');
+		console.log('User disconnected: '+nickname);
+		io.emit('chat message', nickname+' is off');
+		
+
+		index =	users_online.findIndex(function(user){
+			return user.match(nickname)
+		});
+		users_online.splice(index,1);
+		io.emit('user_change', users_online);
 	})
 
 	socket.on('chat message', function(msg){
 		if (nickname == ''){
 			nickname = msg;
-			greet = "Thanks "+nickname+", now you can talk";
+
+			greet = "Thanks "+nickname+", now you're on the loop";
 			socket.emit('chat message', greet);
+			socket.broadcast.emit('chat message', nickname+" is on");
+
+			users_online.push(nickname);
+			io.emit('user_change', users_online);
+
+			console.log('User connected: '+nickname);
 		} else {
 			socket.broadcast.emit('chat message', nickname+': '+msg);
 		}
 	});
 
 	socket.on('typing', function(stat){
-		console.log('typing..');
-		io.emit('typing', stat)
+		socket.broadcast.emit('typing', stat, nickname)
 	})
 });
 
